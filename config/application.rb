a498dfa275cd7bf51eba4a1f70db3f52adce9c1c
Andrea Thacker <andrea_mails@yahoo.com>
2024-12-03 22:26:59 -0600

    Set uuid as primary key type

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module FireHydrant
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use UUIDs as the default primary key type
+    config.active_record.primary_key_type = :uuid
   end
 end
