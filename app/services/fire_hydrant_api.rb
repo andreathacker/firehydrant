5099deb1fe8de1dfc0a2976bf7b75128eb72632f
Andrea Thacker <andreathackermails@gmail.com>
2024-12-04 12:55:50 -0600

    Add FireHydrantApi (#5)

---
+++ app/services/fire_hydrant_api.rb
@@ -0,0 +1,23 @@
+require "httparty"
+
+module Services
+  API_URL = "https://api.firehydrant.io/v1/"
+
+  class FireHydrantApi
+    def initialize(http_client: HTTParty)
+      @http_client = http_client
+    end
+
+    def get_incident_events(incident_id)
+      # TODO: Add error handling
+      response = @http_client.get(
+        "#{API_URL}/incidents/#{incident_id}/events",
+        headers: { "Authorization" => ENV["FIRE_HYDRANT_API_KEY"] }
+      )
+      response.parsed_response
+    end
+  end
+end
+
+# f = Services::FireHydrantApi.new
+# f.get_incident_events("ac5e2480-f51e-47d2-9329-31be9d7dd1c3")
