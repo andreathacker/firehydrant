6c0ea793649a91504a12fa0cc050cc9fe8f71e7f
Andrea Thacker <andrea_mails@yahoo.com>
2024-12-04 20:48:15 -0600

    clues-controller: update incident clues

--- app/services/incident_clues.rb
+++ app/services/incident_clues.rb
@@ -1,20 +1,22 @@
 class IncidentClues
-  def initialize(fire_hydrant_api: FireHydrantApi)
+  def initialize(fire_hydrant_api: FireHydrantApi, clue_model: Clue)
     @fire_hydrant_api = fire_hydrant_api
+    @clue_model = clue_model
   end
 
   def self.build
     IncidentClues.new(
-      fire_hydrant_api: FireHydrantApi.build
+      fire_hydrant_api: FireHydrantApi.build,
+      clue_model: Clue
     )
   end
 
-  def index(incident_id)
+  def list_clues(incident_id)
     incident_events = @fire_hydrant_api.get_incident_events(incident_id)["data"]
 
     event_clues = []
     incident_events.each do |event|
-      if Clue.exists?(event_id: event["id"])
+      if @clue_model.exists?(event_id: event["id"])
         event_clues.append(event)
       end
     end
